9e1155bf112f23b4cc1ba57bd85f6fe379477ae7
david <palandridavid93@gmail.com>
2023-11-02 14:47:04 -0300

    Se resolvieron las 4 querys

--- Tabla nueva 2-11.sql
+++ Tabla nueva 2-11.sql
@@ -20,3 +20,43 @@ from Clientes
 
 select *
 from Viaje_Envio
+
+--- Segunda Query---
+SELECT
+    VE.Codigo_de_viaje AS Codigo_de_viaje,
+    C.Nombre AS Cliente,
+    CH.Nombre AS Chofer,
+	CIUD.Nombre AS Ciudad_de_Origen,
+    VE.Cantidad_de_km_recorridos AS Km_Recorridos,
+    VE.Fecha_Salida_Real AS Fecha_Salida
+FROM dbo.Viaje_Envio VE
+LEFT JOIN dbo.Clientes C ON VE.ID_Cliente = C.ID_Cliente
+LEFT JOIN dbo.Chofer CH ON VE.ID_Chofer = CH.ID_Chofer
+LEFT JOIN dbo.Ciudad CIUD ON VE.ID_Ciudad_Origen = CIUD.ID_Ciudad
+LEFT JOIN dbo.Provincia PROV ON VE.ID_Provincia_Origen = PROV.ID_Provincia
+WHERE VE.Fecha_Salida_Real >= '2023-01-01' AND VE.Fecha_Salida_Real <= '2023-06-30'
+    AND PROV.Nombre = 'Córdoba';
+
+
+--- Tercera Query---
+SELECT TOP 3
+    CH.Nombre AS Nombre_Chofer,
+    SUM(VE.Cantidad_de_km_recorridos) AS Total_Kilometros_Recorridos
+FROM dbo.Chofer CH
+LEFT JOIN dbo.Viaje_Envio VE ON CH.ID_Chofer = VE.ID_Chofer
+WHERE YEAR(VE.Fecha_Salida_Real) = 2023
+GROUP BY CH.Nombre
+ORDER BY Total_Kilometros_Recorridos DESC;
+
+---Cuarta Query---
+SELECT DISTINCT
+    C.Nombre AS Nombre_Cliente,
+	C.Razon_Social AS Compania,
+    CH.Nombre AS Nombre_Chofer,
+    VE.Codigo_de_viaje AS Codigo_de_Viaje,
+    VE.Cantidad_de_km_recorridos AS Km_Recorridos
+FROM dbo.Clientes C
+LEFT JOIN dbo.Viaje_Envio VE ON C.ID_Cliente = VE.ID_Cliente
+LEFT JOIN dbo.Chofer CH ON VE.ID_Chofer = CH.ID_Chofer
+WHERE YEAR(VE.Fecha_Salida_Real) = 2023
+ORDER BY Km_Recorridos DESC;
--- Tablas Creadas con datos insertados.sql
+++ Tablas Creadas con datos insertados.sql
@@ -1,4 +1,4 @@
-USE [SpeedyGonzalesSRL]
+USE SpeedyGonzalesSRL
 
 
 CREATE TABLE dbo.Provincia (
@@ -52,6 +52,7 @@ CREATE TABLE dbo.Clientes(
 
 CREATE TABLE dbo.Chofer(
 	ID_Chofer int NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+	ID_Camion int NOT NULL,
 	Nombre varchar(50) NOT NULL,
 	Apellido varchar(50) NOT NULL,
 	Razon_Social varchar(50) NOT NULL,
@@ -60,13 +61,13 @@ CREATE TABLE dbo.Chofer(
 	Domicilio varchar(50),
 	Telefono varchar(20),
 	Email varchar(50),
+  FOREIGN KEY (ID_Camion) REFERENCES dbo.Camiones (ID_Camion),
 );
 
 CREATE TABLE dbo.Viaje_Envio(
 	ID_Viaje_Envio int NOT NULL IDENTITY(1, 1) PRIMARY KEY,
 	Codigo_de_viaje varchar(50) NOT NULL,
 	ID_Cliente int NOT NULL,
-	ID_Camion int NOT NULL,
 	ID_Chofer int NOT NULL,
 	ID_Provincia_Origen int NOT NULL,
 	ID_Ciudad_Origen int NOT NULL,
@@ -80,7 +81,6 @@ CREATE TABLE dbo.Viaje_Envio(
 	Fecha_Llegada_Estimada DATE NOT NULL,
 	Fecha_Llegada_Real DATE NOT NULL,
  FOREIGN KEY (ID_Cliente) REFERENCES dbo.Clientes (ID_Cliente),
- FOREIGN KEY (ID_Camion) REFERENCES dbo.Camiones (ID_Camion),
  FOREIGN KEY (ID_Chofer) REFERENCES dbo.Chofer (ID_Chofer),
  FOREIGN KEY (ID_Provincia_Origen) REFERENCES dbo.Provincia (ID_Provincia),
  FOREIGN KEY (ID_Ciudad_Origen) REFERENCES dbo.Ciudad (ID_Ciudad),
@@ -107,18 +107,18 @@ VALUES
     ('PQR678', 'Marca6', 2022, '2022-03-05', 3);
 
 /* Inserto datos para 10 choferes */
-INSERT INTO dbo.Chofer (Nombre, Apellido, Razon_Social, DNI, CUIT, Domicilio, Telefono, Email)
+INSERT INTO dbo.Chofer (ID_Camion, Nombre, Apellido, Razon_Social, DNI, CUIT, Domicilio, Telefono, Email)
 VALUES
-    ('Juan', 'Pérez', 'Transportes Pérez SRL', 12345678, '20-12345678-4', 'Dirección1', '555-123-456', 'juan@email.com'),
-    ('Ana', 'López', 'Transportes López SA', 87654321, '27-87654321-8', 'Dirección2', '555-987-654', 'ana@email.com'),
-    ('Carlos', 'Gómez', 'Gómez Transport', 98765432, '30-98765432-6', 'Dirección3', '555-345-678', 'carlos@email.com'),
-    ('María', 'Martínez', 'Martínez Logística', 23456789, '25-23456789-1', 'Dirección4', '555-234-567', 'maria@email.com'),
-    ('Luis', 'Torres', 'Transportes Torres', 34567890, '27-34567890-3', 'Dirección5', '555-789-012', 'luis@email.com'),
-    ('Ana', 'Rodríguez', 'Rodríguez Logistics', 76543210, '30-76543210-7', 'Dirección6', '555-345-678', 'ana2@email.com'),
-    ('Miguel', 'Sánchez', 'Sánchez Transport', 65432109, '25-65432109-9', 'Dirección7', '555-654-321', 'miguel@email.com'),
-    ('Laura', 'García', 'García Transporte', 89012345, '27-89012345-2', 'Dirección8', '555-890-123', 'laura@email.com'),
-    ('Javier', 'Fernández', 'Fernández Trucking', 12345987, '30-12345987-5', 'Dirección9', '555-987-123', 'javier@email.com'),
-    ('Eva', 'Díaz', 'Díaz Logistics', 98765412, '25-98765412-0', 'Dirección10', '555-987-654', 'eva@email.com');
+    (1, 'Juan', 'Pérez', 'Transportes Pérez SRL', 12345678, '20-12345678-4', 'Dirección1', '555-123-456', 'juan@email.com'),
+    (2, 'Ana', 'López', 'Transportes López SA', 87654321, '27-87654321-8', 'Dirección2', '555-987-654', 'ana@email.com'),
+    (3, 'Carlos', 'Gómez', 'Gómez Transport', 98765432, '30-98765432-6', 'Dirección3', '555-345-678', 'carlos@email.com'),
+    (4, 'María', 'Martínez', 'Martínez Logística', 23456789, '25-23456789-1', 'Dirección4', '555-234-567', 'maria@email.com'),
+    (5, 'Luis', 'Torres', 'Transportes Torres', 34567890, '27-34567890-3', 'Dirección5', '555-789-012', 'luis@email.com'),
+    (6, 'Ana', 'Rodríguez', 'Rodríguez Logistics', 76543210, '30-76543210-7', 'Dirección6', '555-345-678', 'ana2@email.com'),
+    (1, 'Miguel', 'Sánchez', 'Sánchez Transport', 65432109, '25-65432109-9', 'Dirección7', '555-654-321', 'miguel@email.com'),
+    (2, 'Laura', 'García', 'García Transporte', 89012345, '27-89012345-2', 'Dirección8', '555-890-123', 'laura@email.com'),
+    (3, 'Javier', 'Fernández', 'Fernández Trucking', 12345987, '30-12345987-5', 'Dirección9', '555-987-123', 'javier@email.com'),
+    (4, 'Eva', 'Díaz', 'Díaz Logistics', 98765412, '25-98765412-0', 'Dirección10', '555-987-654', 'eva@email.com');
 
 
 
@@ -223,25 +223,25 @@ VALUES
 
 
 /* INSERTO 18 VIAJES */
-INSERT INTO dbo.Viaje_Envio (Codigo_de_viaje, ID_Cliente, ID_Camion, ID_Chofer, ID_Provincia_Origen, ID_Ciudad_Origen, Dirección_Origen, ID_Provincia_Destino, ID_Ciudad_Destino, Dirección_Destino, Cantidad_de_km_recorridos, Fecha_Salida_Estimada, Fecha_Salida_Real, Fecha_Llegada_Estimada, Fecha_Llegada_Real)
+INSERT INTO dbo.Viaje_Envio (Codigo_de_viaje, ID_Cliente, ID_Chofer, ID_Provincia_Origen, ID_Ciudad_Origen, Dirección_Origen, ID_Provincia_Destino, ID_Ciudad_Destino, Dirección_Destino, Cantidad_de_km_recorridos, Fecha_Salida_Estimada, Fecha_Salida_Real, Fecha_Llegada_Estimada, Fecha_Llegada_Real)
 VALUES
-    ('VIAJE001', 1, 2, 1, 1, 1, 'Dirección Origen 1', 1, 2, 'Dirección Destino 1', 500.5, '2023-10-31', '2023-10-31', '2023-11-05', '2023-11-05'),
-    ('VIAJE002', 2, 2, 2, 2, 3, 'Dirección Origen 2', 2, 4, 'Dirección Destino 2', 600.0, '2023-11-01', '2023-11-01', '2023-11-06', '2023-11-06'),
-    ('VIAJE003', 3, 3, 3, 3, 5, 'Dirección Origen 3', 3, 6, 'Dirección Destino 3', 700.2, '2023-11-02', '2023-11-02', '2023-11-07', '2023-11-07'),
-    ('VIAJE004', 4, 3, 4, 4, 7, 'Dirección Origen 4', 4, 8, 'Dirección Destino 4', 800.7, '2023-11-03', '2023-11-03', '2023-11-08', '2023-11-08'),
-    ('VIAJE005', 5, 3, 5, 5, 9, 'Dirección Origen 5', 5, 10, 'Dirección Destino 5', 900.0, '2023-11-04', '2023-11-04', '2023-11-09', '2023-11-09'),
-    ('VIAJE006', 6, 4, 6, 1, 11, 'Dirección Origen 6', 2, 12, 'Dirección Destino 6', 600.0, '2023-11-05', '2023-11-05', '2023-11-10', '2023-11-10'),
-    ('VIAJE007', 7, 4, 7, 2, 13, 'Dirección Origen 7', 3, 14, 'Dirección Destino 7', 700.5, '2023-11-06', '2023-11-06', '2023-11-11', '2023-11-11'),
-    ('VIAJE008', 8, 4, 8, 3, 15, 'Dirección Origen 8', 4, 16, 'Dirección Destino 8', 800.2, '2023-11-07', '2023-11-07', '2023-11-12', '2023-11-12'),
-    ('VIAJE009', 9, 5, 9, 4, 17, 'Dirección Origen 9', 5, 18, 'Dirección Destino 9', 900.8, '2023-11-08', '2023-11-08', '2023-11-13', '2023-11-13'),
-	('VIAJE010', 10, 5, 2, 2, 1, 'Dirección Origen 10', 2, 3, 'Dirección Destino 10', 550.0, '2023-11-09', '2023-11-09', '2023-11-14', '2023-11-14'),
-    ('VIAJE011', 11, 5, 3, 3, 2, 'Dirección Origen 11', 3, 4, 'Dirección Destino 11', 620.0, '2023-11-10', '2023-11-10', '2023-11-15', '2023-11-15'),
-    ('VIAJE012', 12, 6, 4, 4, 3, 'Dirección Origen 12', 4, 5, 'Dirección Destino 12', 720.2, '2023-11-11', '2023-11-11', '2023-11-16', '2023-11-16'),
-    ('VIAJE013', 13, 6, 5, 5, 4, 'Dirección Origen 13', 5, 6, 'Dirección Destino 13', 820.7, '2023-11-12', '2023-11-12', '2023-11-17', '2023-11-17'),
-    ('VIAJE014', 14, 6, 6, 1, 5, 'Dirección Origen 14', 2, 7, 'Dirección Destino 14', 920.0, '2023-11-13', '2023-11-13', '2023-11-18', '2023-11-18'),
-    ('VIAJE015', 15, 6, 7, 2, 6, 'Dirección Origen 15', 3, 8, 'Dirección Destino 15', 560.5, '2023-11-14', '2023-11-14', '2023-11-19', '2023-11-19'),
-    ('VIAJE016', 16, 7, 8, 3, 7, 'Dirección Origen 16', 4, 9, 'Dirección Destino 16', 840.2, '2023-11-15', '2023-11-15', '2023-11-20', '2023-11-20'),
-    ('VIAJE017', 17, 7, 9, 4, 8, 'Dirección Origen 17', 5, 10, 'Dirección Destino 17', 920.8, '2023-11-16', '2023-11-16', '2023-11-21', '2023-11-21'),
-    ('VIAJE018', 19, 7, 3, 5, 2, 'Dirección Origen 19', 1, 1, 'Dirección Destino 19', 550.0, '2023-11-17', '2023-11-17', '2023-11-22', '2023-11-22'),
-	('VIAJE019', 20, 2, 4, 2, 3, 'Dirección Origen 20', 2, 4, 'Dirección Destino 20', 670.0, '2023-11-18', '2023-11-18', '2023-11-23', '2023-11-23'),
-    ('VIAJE020', 10, 3, 1, 1, 2, 'Dirección Origen 10', 2, 3, 'Dirección Destino 10', 610.0, '2023-11-19', '2023-11-19', '2023-11-24', '2023-11-24');
+    ('VIAJE001', 1, 1, 1, 1, 'Dirección Origen 1', 1, 2, 'Dirección Destino 1', 500.5, '2023-10-31', '2023-10-31', '2023-11-05', '2023-11-05'),
+    ('VIAJE002', 2, 2, 2, 3, 'Dirección Origen 2', 2, 4, 'Dirección Destino 2', 600.0, '2023-11-01', '2023-04-01', '2023-11-06', '2023-11-06'),
+    ('VIAJE003', 3, 3, 3, 5, 'Dirección Origen 3', 3, 6, 'Dirección Destino 3', 700.2, '2023-11-02', '2023-11-02', '2023-11-07', '2023-11-07'),
+    ('VIAJE004', 4, 4, 4, 7, 'Dirección Origen 4', 4, 8, 'Dirección Destino 4', 800.7, '2023-11-03', '2023-11-03', '2023-11-08', '2023-11-08'),
+    ('VIAJE005', 5, 5, 5, 9, 'Dirección Origen 5', 5, 10, 'Dirección Destino 5', 900.0, '2023-11-04', '2023-03-04', '2023-11-09', '2023-11-09'),
+    ('VIAJE006', 6, 6, 1, 11, 'Dirección Origen 6', 2, 12, 'Dirección Destino 6', 600.0, '2023-11-05', '2023-11-05', '2023-11-10', '2023-11-10'),
+    ('VIAJE007', 7, 7, 2, 13, 'Dirección Origen 7', 3, 14, 'Dirección Destino 7', 700.5, '2023-11-06', '2023-11-06', '2023-11-11', '2023-11-11'),
+    ('VIAJE008', 8, 8, 3, 15, 'Dirección Origen 8', 4, 16, 'Dirección Destino 8', 800.2, '2023-11-07', '2023-11-07', '2023-11-12', '2023-11-12'),
+    ('VIAJE009', 9, 9, 4, 17, 'Dirección Origen 9', 5, 18, 'Dirección Destino 9', 900.8, '2023-11-08', '2023-11-08', '2023-11-13', '2023-11-13'),
+	('VIAJE010', 10, 2, 2, 1, 'Dirección Origen 10', 2, 3, 'Dirección Destino 10', 550.0, '2023-11-09', '2023-11-09', '2023-11-14', '2023-11-14'),
+    ('VIAJE011', 11, 3, 3, 2, 'Dirección Origen 11', 3, 4, 'Dirección Destino 11', 620.0, '2023-11-10', '2023-11-10', '2023-11-15', '2023-11-15'),
+    ('VIAJE012', 12, 4, 4, 3, 'Dirección Origen 12', 20, 5, 'Dirección Destino 12', 720.2, '2023-11-11', '2023-11-11', '2023-11-16', '2023-11-16'),
+    ('VIAJE013', 13, 5, 5, 4, 'Dirección Origen 13', 5, 6, 'Dirección Destino 13', 820.7, '2023-11-12', '2023-11-12', '2023-11-17', '2023-11-17'),
+    ('VIAJE014', 14, 6, 1, 5, 'Dirección Origen 14', 2, 7, 'Dirección Destino 14', 920.0, '2023-11-13', '2023-11-13', '2023-11-18', '2023-11-18'),
+    ('VIAJE015', 15, 7, 2, 6, 'Dirección Origen 15', 20, 8, 'Dirección Destino 15', 560.5, '2023-11-14', '2023-11-14', '2023-11-19', '2023-11-19'),
+    ('VIAJE016', 16, 8, 3, 7, 'Dirección Origen 16', 4, 9, 'Dirección Destino 16', 840.2, '2023-11-15', '2023-11-15', '2023-11-20', '2023-11-20'),
+    ('VIAJE017', 17, 9, 4, 8, 'Dirección Origen 17', 20, 10, 'Dirección Destino 17', 920.8, '2023-11-16', '2023-11-16', '2023-11-21', '2023-11-21'),
+    ('VIAJE018', 19, 3, 5, 2, 'Dirección Origen 19', 1, 1, 'Dirección Destino 19', 550.0, '2023-11-17', '2023-11-17', '2023-11-22', '2023-11-22'),
+	('VIAJE019', 20, 4, 2, 3, 'Dirección Origen 20', 2, 4, 'Dirección Destino 20', 670.0, '2023-11-18', '2023-11-18', '2023-11-23', '2023-11-23'),
+    ('VIAJE020', 10, 1, 1, 2, 'Dirección Origen 10', 20, 3, 'Dirección Destino 10', 610.0, '2023-11-19', '2023-11-19', '2023-11-24', '2023-11-24');
