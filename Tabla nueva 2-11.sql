6083419f8ce5ac1095afc2e52e3819112300d58c
david <palandridavid93@gmail.com>
2023-11-06 22:15:17 -0300

    gg

--- Tabla nueva 2-11.sql
+++ Tabla nueva 2-11.sql
@@ -1,6 +1,7 @@
-select COUNT(*) 
+select COUNT(*) AS Viajes_a_Provincia_StFe
 from Viaje_Envio v
-left join Provincia p ON v.ID_Provincia_Destino = p.ID_Provincia
+left join Ciudad c ON v.ID_Ciudad_Destino = c.ID_Ciudad
+left join Provincia p ON c.ID_Provincia = p.ID_Provincia
 WHERE P.Nombre = 'Santa Fe'; 
 
 select * 
@@ -19,34 +20,45 @@ select *
 from Clientes
 
 select *
+from Marcas
+
+select *
+from Modelos
+
+select *
 from Viaje_Envio
 
 --- Segunda Query---
 SELECT
-    VE.Codigo_de_viaje AS Codigo_de_viaje,
+    v.Codigo_de_viaje AS Codigo_de_viaje,
     C.Nombre AS Cliente,
     CH.Nombre AS Chofer,
 	CIUD.Nombre AS Ciudad_de_Origen,
-    VE.Cantidad_de_km_recorridos AS Km_Recorridos,
-    VE.Fecha_Salida_Real AS Fecha_Salida
-FROM dbo.Viaje_Envio VE
-LEFT JOIN dbo.Clientes C ON VE.ID_Cliente = C.ID_Cliente
-LEFT JOIN dbo.Chofer CH ON VE.ID_Chofer = CH.ID_Chofer
-LEFT JOIN dbo.Ciudad CIUD ON VE.ID_Ciudad_Origen = CIUD.ID_Ciudad
-LEFT JOIN dbo.Provincia PROV ON VE.ID_Provincia_Origen = PROV.ID_Provincia
-WHERE VE.Fecha_Salida_Real >= '2023-01-01' AND VE.Fecha_Salida_Real <= '2023-06-30'
-    AND PROV.Nombre = 'Córdoba';
+    v.Cantidad_de_km_recorridos AS Km_Recorridos,
+    v.Fecha_Salida_Real AS Fecha_Salida,
+	v.Fecha_Llegada_Real AS Fecha_Llegada
+FROM dbo.Viaje_Envio v
+LEFT JOIN Clientes c ON v.ID_Cliente = c.ID_Cliente
+LEFT JOIN Chofer ch ON v.ID_Chofer = ch.ID_Chofer
+LEFT JOIN Ciudad ciud ON v.ID_Ciudad_Origen = ciud.ID_Ciudad
+LEFT JOIN Provincia p ON ciud.ID_Provincia = p.ID_Provincia
+WHERE v.Fecha_Salida_Real >= '2023-01-01' AND v.Fecha_Salida_Real <= '2023-06-30'
+    AND p.Nombre = 'Córdoba';
 
+-- Tomamos los viajes que salieron en el primer semestre sin comtemplar la fecha de llegada :-).
+-- El formato de fecha es  "AAAA-MM-DD" 
 
 --- Tercera Query---
 SELECT TOP 3
-    CH.Nombre AS Nombre_Chofer,
-    SUM(VE.Cantidad_de_km_recorridos) AS Total_Kilometros_Recorridos
-FROM dbo.Chofer CH
-LEFT JOIN dbo.Viaje_Envio VE ON CH.ID_Chofer = VE.ID_Chofer
-WHERE YEAR(VE.Fecha_Salida_Real) = 2023
-GROUP BY CH.Nombre
+    ch.Nombre AS Nombre_Chofer,
+    SUM(v.Cantidad_de_km_recorridos) AS Total_Kilometros_Recorridos
+FROM dbo.Chofer ch
+LEFT JOIN dbo.Viaje_Envio v ON ch.ID_Chofer = v.ID_Chofer
+WHERE YEAR(v.Fecha_Salida_Real) = 2023
+GROUP BY ch.Nombre
 ORDER BY Total_Kilometros_Recorridos DESC;
+
+-- SELECT YEAR(GETDATE()) AS Año;
 
 ---Cuarta Query---
 SELECT DISTINCT
