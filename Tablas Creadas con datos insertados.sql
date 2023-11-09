55ed7a55e9a05c0f2c172b37ce54200ec24a8127
FefeTroanes <ftroanes@gmail.com>
2023-11-08 21:50:25 -0300

    Se modificaron los atributos de las entidades
    Signed-off-by: FefeTroanes <ftroanes@gmail.com>

--- Tablas Creadas con datos insertados.sql
+++ Tablas Creadas con datos insertados.sql
@@ -1,105 +1,110 @@
+-- 2. Escriba las instrucciones SQL para la creaciï¿½n de las tablas correspondientes al
+-- diagrama anterior.
+
 CREATE DATABASE SpeedyGonzalesSRL;
 
 USE SpeedyGonzalesSRL;
 
-
-CREATE TABLE dbo.Provincia (
-    ID_Provincia int NOT NULL IDENTITY(1, 1) PRIMARY KEY,
-    Nombre varchar(50)
+CREATE TABLE Provincia (
+	ID_Provincia tinyint NOT NULL IDENTITY PRIMARY KEY,
+	Nombre varchar(50)
 );
 
-
-CREATE TABLE dbo.Ciudad(
-	ID_Ciudad int NOT NULL IDENTITY(1, 1) PRIMARY KEY,
-	ID_Provincia int NOT NULL,
+CREATE TABLE Ciudad (
+	ID_Ciudad int NOT NULL IDENTITY PRIMARY KEY,
+	ID_Provincia tinyint NOT NULL,
 	Nombre varchar(50),
-	FOREIGN KEY (ID_Provincia) REFERENCES dbo.Provincia (ID_Provincia)
+	FOREIGN KEY (ID_Provincia) REFERENCES Provincia (ID_Provincia)
 );
 
-
-CREATE TABLE dbo.Remolques(
-	ID_Remolques int NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+CREATE TABLE Remolque (
+	ID_Remolque tinyint NOT NULL IDENTITY PRIMARY KEY,
 	Tipo_de_remolque varchar(50) NOT NULL,
 );
 
-
-CREATE TABLE dbo.Camiones(
-	ID_Camion int NOT NULL IDENTITY(1, 1) PRIMARY KEY,
-	Patente varchar(50),
-	Marca varchar(50),
-	Modelo int,
-	Año DATE,
-	ID_Remolque int NOT NULL,
-	FOREIGN KEY (ID_Remolque) REFERENCES dbo.Remolques (ID_Remolques)
+CREATE TABLE dbo.Marcas(
+	ID_Marca int NOT NULL IDENTITY PRIMARY KEY,
+	Nombre_Marca varchar(50),
 );
 
+CREATE TABLE dbo.Modelos(
+	ID_Modelos int NOT NULL IDENTITY PRIMARY KEY,
+	Nombre_Modelos varchar(50),
+	ID_Marca int NOT NULL,
+	FOREIGN KEY (ID_Marca) REFERENCES Marcas (ID_Marca)
+);
 
-CREATE TABLE dbo.Clientes(
-	ID_Cliente int NOT NULL IDENTITY(1, 1) PRIMARY KEY,
-	Codigo_de_viaje AS ('CLI' + RIGHT('000' + CAST(ID_Cliente AS VARCHAR(3)), 4)) PERSISTED,
+CREATE TABLE Camion (
+	ID_Camion int NOT NULL IDENTITY PRIMARY KEY,
+	Patente varchar(30),
+	Marca varchar(40),
+	ID_Modelo int NOT NULL,
+	Anio YEAR,
+	ID_Remolque tinyint NOT NULL,
+	FOREIGN KEY (ID_Remolque) REFERENCES Remolque (ID_Remolque),
+	FOREIGN KEY (ID_Modelo) REFERENCES dbo.Modelos (ID_Modelos)
+);
+
+CREATE TABLE Cliente (
+	ID_Cliente int NOT NULL IDENTITY PRIMARY KEY,
+	Codigo_de_cliente AS ('CLI' + RIGHT('000' + CAST(ID_Cliente AS VARCHAR(3)), 4)) PERSISTED,
 	Nombre varchar(50),
 	Apellido varchar(50),
-	DNI int,
 	Razon_Social varchar(50),
+	DNI int,
 	CUIT varchar(50),
-	Telefono varchar(50),
-	Email varchar(50),
-	ID_Ciudad int,
-	ID_Provincia int,
 	Domicilio varchar(50),
-	FOREIGN KEY (ID_Ciudad) REFERENCES dbo.Ciudad (ID_Ciudad),
-	FOREIGN KEY (ID_Provincia) REFERENCES dbo.Provincia (ID_Provincia)
+	Telefono varchar(30),
+	Email varchar(50),
+	ID_Ciudad int NOT NULL,
+	FOREIGN KEY (ID_Ciudad) REFERENCES Ciudad (ID_Ciudad),
 );
 
-
-CREATE TABLE dbo.Chofer(
-	ID_Chofer int NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+CREATE TABLE Chofer (
+	ID_Chofer int NOT NULL IDENTITY PRIMARY KEY,
 	ID_Camion int NOT NULL,
 	Nombre varchar(50) NOT NULL,
 	Apellido varchar(50) NOT NULL,
-	Razon_Social varchar(50) NOT NULL,
 	DNI int NOT NULL,
-	CUIT varchar(20) NOT NULL,
 	Domicilio varchar(50),
-	Telefono varchar(20),
+	Telefono_fijo varchar(30),
+	Telefono_celular varchar(30),
+	Edad tinyint,
 	Email varchar(50),
-  FOREIGN KEY (ID_Camion) REFERENCES dbo.Camiones (ID_Camion),
+	Codigo_de_registro varchar(50),
+  FOREIGN KEY (ID_Camion) REFERENCES Camiones (ID_Camion),
 );
 
-CREATE TABLE dbo.Viaje_Envio(
-	ID_Viaje_Envio int NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+CREATE TABLE Viaje_Envio(
+	ID_Viaje_Envio int NOT NULL IDENTITY PRIMARY KEY,
 	Codigo_de_viaje AS ('VIAJE' + RIGHT('000' + CAST(ID_Viaje_Envio AS VARCHAR(3)), 4)) PERSISTED,
 	ID_Cliente int NOT NULL,
 	ID_Chofer int NOT NULL,
-	--ID_Provincia_Origen int NOT NULL,
 	ID_Ciudad_Origen int NOT NULL,
-	Dirección_Origen varchar(50),
-	--ID_Provincia_Destino int NOT NULL,
+	Direccion_Origen varchar(50),
 	ID_Ciudad_Destino int NOT NULL,
-	Dirección_Destino varchar(50),
-	Cantidad_de_km_recorridos REAL NOT NULL,
+	Direccion_Destino varchar(50),
+	Cantidad_de_km_recorridos DECIMAL(10, 2) NOT NULL,
 	Fecha_Salida_Estimada DATE NOT NULL,
-	Fecha_Salida_Real DATE NOT NULL,
+	Fecha_Salida_Real DATE,
 	Fecha_Llegada_Estimada DATE NOT NULL,
-	Fecha_Llegada_Real DATE NOT NULL,
- FOREIGN KEY (ID_Cliente) REFERENCES dbo.Clientes (ID_Cliente),
- FOREIGN KEY (ID_Chofer) REFERENCES dbo.Chofer (ID_Chofer),
- --FOREIGN KEY (ID_Provincia_Origen) REFERENCES dbo.Provincia (ID_Provincia),
- FOREIGN KEY (ID_Ciudad_Origen) REFERENCES dbo.Ciudad (ID_Ciudad),
- --FOREIGN KEY (ID_Provincia_Destino) REFERENCES dbo.Provincia (ID_Provincia),
- FOREIGN KEY (ID_Ciudad_Destino) REFERENCES dbo.Ciudad (ID_Ciudad),
+	Fecha_Llegada_Real DATE,
+	FOREIGN KEY (ID_Cliente) REFERENCES Clientes (ID_Cliente),
+	FOREIGN KEY (ID_Chofer) REFERENCES Chofer (ID_Chofer),
+	FOREIGN KEY (ID_Ciudad_Origen) REFERENCES Ciudad (ID_Ciudad),
+	FOREIGN KEY (ID_Ciudad_Destino) REFERENCES Ciudad (ID_Ciudad),
 );
 
 
 /* Inserto tres remolques */
 INSERT INTO dbo.Remolques (Tipo_de_remolque)
 VALUES
-    ('Remolque de enganche'),
-    ('Remolque Eje central'),
-    ('Remolque Semiremolque');
+	('Remolque de enganche'),
+	('Remolque Eje central'),
+	('Remolque Semiremolque');
 
 /* Inserto datos para 6 Camiones*/
-INSERT INTO dbo.Camiones (Patente, Marca, Modelo, Año, ID_Remolque)
+INSERT INTO dbo.Camiones (Patente, Marca, Modelo, Anio, ID_Remolque)
 VALUES
     ('ABC123', 'Marca1', 2020, '2020-01-01', 1),
     ('DEF456', 'Marca2', 2019, '2019-02-15', 2),
@@ -109,8 +114,8 @@ VALUES
 /* Inserto datos para 10 choferes */
 INSERT INTO dbo.Chofer (ID_Camion, Nombre, Apellido, Razon_Social, DNI, CUIT, Domicilio, Telefono, Email)
 VALUES
-    (1, 'Juan', 'Pérez', 'Transportes Pérez SRL', 12345678, '20-12345678-4', 'Dirección1', '555-123-456', 'juan@email.com'),
-    (2, 'Ana', 'López', 'Transportes López SA', 87654321, '27-87654321-8', 'Dirección2', '555-987-654', 'ana@email.com');
+    (1, 'Juan', 'Pï¿½rez', 'Transportes Pï¿½rez SRL', 12345678, '20-12345678-4', 'Direcciï¿½n1', '555-123-456', 'juan@email.com'),
+    (2, 'Ana', 'Lï¿½pez', 'Transportes Lï¿½pez SA', 87654321, '27-87654321-8', 'Direcciï¿½n2', '555-987-654', 'ana@email.com');
 
 
 
@@ -119,17 +124,17 @@ VALUES ('Buenos Aires'),
 ('Catamarca'),
 ('Chaco'),
 ('Chubut'),
-('Córdoba'),
+('Cï¿½rdoba'),
 ('Corrientes'),
-('Entre Ríos'),
+('Entre Rï¿½os'),
 ('Formosa'),
 ('Jujuy'),
 ('La Pampa'),
 ('La Rioja'),
 ('Mendoza'),
 ('Misiones'),
-('Neuquén'),
-('Río Negro'),
+('Neuquï¿½n'),
+('Rï¿½o Negro'),
 ('Salta'),
 ('San Juan'),
 ('San Luis'),
@@ -137,15 +142,15 @@ VALUES ('Buenos Aires'),
 ('Santa Fe'),
 ('Santiago del Estero'),
 ('Tierra del Fuego'),
-('Tucumán');
+('Tucumï¿½n');
 
 /* Inserto 50 ciudades de Argentina con su ID_provincia*/
 INSERT INTO dbo.Ciudad (ID_Provincia, Nombre)
 VALUES
     (1, 'Buenos Aires'),
     (1, 'La Plata'),
-    (5, 'Córdoba'),
-    (5, 'Villa María'),
+    (5, 'Cï¿½rdoba'),
+    (5, 'Villa Marï¿½a'),
     (20, 'Rosario'),
     (20, 'Santa Fe');
 
@@ -153,14 +158,14 @@ VALUES
 /* Inserto 20 Clientes entre personas y empresas*/
 INSERT INTO dbo.Clientes (Nombre, Apellido, DNI, Razon_Social, CUIT, Telefono, Email, ID_Ciudad, Domicilio)
 VALUES
-    ('Juan', 'Pérez', 12345678, NULL, NULL, '555-123-456', 'juan@email.com', 1, 'Dirección1'),
-    ('Ana', 'López', 87654321, NULL, NULL, '555-987-654', 'ana@email.com', 2, 'Dirección2'),
-    ('Carlos', 'Gómez', 98765432, NULL, NULL, '555-345-678', 'carlos@email.com', 3, 'Dirección3');
+    ('Juan', 'Pï¿½rez', 12345678, NULL, NULL, '555-123-456', 'juan@email.com', 1, 'Direcciï¿½n1'),
+    ('Ana', 'Lï¿½pez', 87654321, NULL, NULL, '555-987-654', 'ana@email.com', 2, 'Direcciï¿½n2'),
+    ('Carlos', 'Gï¿½mez', 98765432, NULL, NULL, '555-345-678', 'carlos@email.com', 3, 'Direcciï¿½n3');
 
 
 /* INSERTO 18 VIAJES */
-INSERT INTO dbo.Viaje_Envio (ID_Cliente, ID_Chofer, ID_Ciudad_Origen, Dirección_Origen, ID_Ciudad_Destino, Dirección_Destino, Cantidad_de_km_recorridos, Fecha_Salida_Estimada, Fecha_Salida_Real, Fecha_Llegada_Estimada, Fecha_Llegada_Real)
+INSERT INTO dbo.Viaje_Envio (ID_Cliente, ID_Chofer, ID_Ciudad_Origen, Direcciï¿½n_Origen, ID_Ciudad_Destino, Direcciï¿½n_Destino, Cantidad_de_km_recorridos, Fecha_Salida_Estimada, Fecha_Salida_Real, Fecha_Llegada_Estimada, Fecha_Llegada_Real)
 VALUES
-    (1, 1, 1, 'Dirección Origen 1', 2, 'Dirección Destino 1', 500.5, '2023-10-31', '2023-10-31', '2023-11-05', '2023-11-05'),
-    (2, 2, 3, 'Dirección Origen 2', 4, 'Dirección Destino 2', 600.0, '2023-11-01', '2023-04-01', '2023-11-06', '2023-11-06'),
-    (3, 2, 5, 'Dirección Origen 3', 6, 'Dirección Destino 3', 700.2, '2023-11-02', '2023-11-02', '2023-11-07', '2023-11-07');
+    (1, 1, 1, 'Direcciï¿½n Origen 1', 2, 'Direcciï¿½n Destino 1', 500.5, '2023-10-31', '2023-10-31', '2023-11-05', '2023-11-05'),
+    (2, 2, 3, 'Direcciï¿½n Origen 2', 4, 'Direcciï¿½n Destino 2', 600.0, '2023-11-01', '2023-04-01', '2023-11-06', '2023-11-06'),
+    (3, 2, 5, 'Direcciï¿½n Origen 3', 6, 'Direcciï¿½n Destino 3', 700.2, '2023-11-02', '2023-11-02', '2023-11-07', '2023-11-07');
